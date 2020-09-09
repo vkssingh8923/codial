b91b2c44f8904327b63086eb78c311877d7645b8
vkssingh892354@gmail.com <65289999+vkssingh8923@users.noreply.github.com>
2020-09-09 01:49:24 -0700

    routing complete

---
+++ controllers/home_controller.js
@@ -0,0 +1,5 @@
+module.exports.home = function (req, res){
+    return res.end("<h1>hello</h1>");
+}
+
+
--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use('/',require('./routes'))
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
