19fdb36a6aa83e4eecc1fe374edc1aaeb0e9f6de
vkssingh892354@gmail.com <65289999+vkssingh8923@users.noreply.github.com>
2020-09-09 05:24:37 -0700

    views added
    ejs adding

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const path =require('path')
+
+app.set('view engine','ejs')
+app.set('views',path.join(__dirname,'views'))
+
 
 app.use('/',require('./routes'))
 
