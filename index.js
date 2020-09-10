fa90b3dfb25ccb4df51daae574aae3212dfaa345
vkssingh892354@gmail.com <65289999+vkssingh8923@users.noreply.github.com>
2020-09-09 19:12:02 -0700

    controllers added

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 const path =require('path')
+const expressejs = require('express-ejs-layouts')
 
 app.set('view engine','ejs')
 app.set('views',path.join(__dirname,'views'))
 
-
+app.use(expressejs)
+app.use(express.static('public'))
 app.use('/',require('./routes'))
+
+
 
 
 app.listen(port, () => {
